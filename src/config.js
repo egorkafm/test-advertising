7b904d3a90f42351f2ee483c7e724aac5df6f192
Yehor Lashchenko <yehorlashchenko@MacBook-Air.local>
2025-02-20 14:46:34 +0700

    refactoring

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const CONFIG = {
+  CTA_URL: "https://example.com"
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { createSlide1, createSlide2 } from "./js/slides";
 import { goToSlide2 } from "./js/animations";
+import { CONFIG } from "./config.js";
 
 createSlide1()
 createSlide2()
@@ -10,8 +11,10 @@ document.querySelector(".slide1-arrow").addEventListener("click", goToSlide2);
 // cta btn
 document.querySelectorAll(".slide1-cta, .slide2-cta").forEach((btn) => {
   btn.addEventListener("click", () => {
-    const destinationUrl = new URL("https://example.com");
+    const destinationUrl = new URL(CONFIG.CTA_URL);
     destinationUrl.searchParams.set("clicktag", '');
     window.location.href = destinationUrl.toString();
   });
 });
+
+
